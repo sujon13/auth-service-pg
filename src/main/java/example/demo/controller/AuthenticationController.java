6b4f791232c50a5a614f20a57b451ac2775f73b3
Arifur Rahman Sujon <arifurrahmansujon27@gmaile.com>
2024-12-03 00:41:23 +0600

    refactored authenticate endpoint

--- src/main/java/example/demo/controller/AuthenticationController.java
+++ src/main/java/example/demo/controller/AuthenticationController.java
@@ -1,12 +1,9 @@
 package example.demo.controller;
 
 import example.demo.model.AuthenticationRequest;
-import example.demo.model.UserResponse;
-import example.demo.service.UserService;
 import example.demo.service.auth.AuthenticationService;
 import example.demo.service.auth.CookieService;
 import example.demo.service.auth.PasswordService;
-import example.demo.signup.model.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -21,17 +18,14 @@ public class AuthenticationController {
     private final PasswordService passwordService;
     private final AuthenticationService authenticationService;
     private final CookieService cookieService;
-    private final UserService userService;
-
 
     @PostMapping("/authenticate")
-    public ResponseEntity<UserResponse> getAuthToken(@Valid @RequestBody AuthenticationRequest request) throws Exception {
+    public ResponseEntity<Void> getAuthToken(@Valid @RequestBody AuthenticationRequest request) throws Exception {
         final String jwtToken = authenticationService.createAuthenticationToken(request);
         final ResponseCookie authCookie = cookieService.buildAuthCookie(jwtToken);
-        final User user = userService.getUserByUserName(request.getUserName()).orElseThrow();
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, authCookie.toString())
-                .body(userService.buildUserResponse(user));
+                .build();
     }
 
     @GetMapping("/password")
