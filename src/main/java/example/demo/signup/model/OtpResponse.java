ea190c3e548f67f2eba1ab62e4bd1f7662b3d2af
Arifur Rahman Sujon <arifurrahmansujon27@gmaile.com>
2024-12-04 02:01:25 +0600

    Removed unwanted import from OtpResponse

--- src/main/java/example/demo/signup/model/OtpResponse.java
+++ src/main/java/example/demo/signup/model/OtpResponse.java
@@ -1,11 +1,8 @@
 package example.demo.signup.model;
 
-import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 
 @Getter
